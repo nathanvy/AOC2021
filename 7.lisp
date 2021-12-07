dea26babc959ac1559074564a6421946ccb99542
nathanvy <n@0x85.org>
2021-12-06 21:37:53 -0800

    mapreduce for the win

---
+++ 7.lisp
@@ -0,0 +1,10 @@
+(load "util.lisp")
+
+(defparameter *testinput* (mapcar #'parse-integer (ppcre:all-matches-as-strings "[0-9]+" (uiop:read-file-string "test7.txt"))))
+(defparameter *input-a* (mapcar #'parse-integer (ppcre:all-matches-as-strings "[0-9]+" (fetch-input 7))))
+
+(defun crunch (input n)
+  (reduce #'+ (mapcar #'(lambda (x)  (abs (- x n))) input)))
+
+(defun crunch-part2 (input n)
+  (reduce #'+ (mapcar #'(lambda (x)  (/ (* (abs (- x n)) (1+ (abs (- x n)))) 2)) input)))
